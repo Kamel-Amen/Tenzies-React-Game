6cb385afbb1b7e90b53c4f069042589f389dbe87
Kamel Amen <kamel_amen@Kamel.Amen>
2024-02-10 19:13:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+# Tenzies-React-Game
+Tenzies is a rolling dice game with new methods and libraries for using with react.
+=======
+# Tenzies || React Game
+
+Tenzies is a rolling dice game with new methods and libraries for using with react.
+
+>>>>>>> 1cc6ea0 (first commit)
---
+++ src/components/Timer.jsx
@@ -0,0 +1,20 @@
+/*
+/* eslint-disable react/prop-types 
+//import React from 'react';
+
+const Timer = ({ duration }) => {
+  const styles = {
+    backgroundColor: duration < 6 ? '#d52b1e' : 'fff',
+    animation: duration < 6 ? 'timeOut 1s ease infinite' : 'none',
+  };
+
+  return (
+    <div className='timer' style={styles}>
+      {duration}
+    </div>
+  );
+};
+
+export default Timer;
+
+*/
